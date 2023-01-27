bf1b638b3b5fc5854f6455eeaa4315373ec817f0
januspaul <januspaul52@gmail.com>
2023-01-27 10:34:27 +0800

    added MemberController

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Member;
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    public function display(){
+        return view('welcome')->with('members',Member::orderByDesc('created_at')->get());
+    }
+
+    public function create(Request $request){
+        $member = new Member;
+        $member->name = $request->name;
+        $member->email = $member->email;
+        $member->save();
+
+
+    }
+
+    public function delete(Request $request){
+        $member = Member::find($request->id);
+        $member->delete();
+    }
+}

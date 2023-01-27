d7cb7885e4c36f6f1b607a0a3d13286cb5f294c2
januspaul <januspaul52@gmail.com>
2023-01-27 10:25:03 +0800

    added members relation

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+    
+    protected $table = "members";
+    
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+    public function trainers(){
+        return $this->hasOne(Trainer::class);
+    }
+    public function membership(){
+        return $this->hasOne(Membership::class);
+    }
+        
+    
 }
